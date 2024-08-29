763ed961cddf12e9f8647f0a411879d347d4a990
bdev-jw <162405371+bdev-jw@users.noreply.github.com>
2024-08-29 15:19:55 +0900

    Update MobileNav.tsx
    Yoom -> MESA 로 수정

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -39,7 +39,7 @@ const MobileNav = () => {
                 className='max-sm:size-10'
                 />
                 <p className='text-[26px] font-extrabold
-                text-white'>Yoom</p>
+                text-white'>MESA</p>
             </Link>
 
             <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
